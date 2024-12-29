b6f295dd43a81f5edad5cbf85b668cc771f9bcb6
Seepick <seepick@users.noreply.github.com>
2024-12-29 21:31:55 +0100

    fix test

--- src/test/kotlin/seepick/localsportsclub/service/DateTimeRangeTest.kt
+++ src/test/kotlin/seepick/localsportsclub/service/DateTimeRangeTest.kt
@@ -11,7 +11,7 @@ class DateTimeRangeTest : StringSpec() {
     private val anyDateTime = LocalDateTime.now()
 
     init {
-        "isMatching for date" {
+        "isStartMatching for date only" {
             val startDate = LocalDate.of(2024, 12, 5)
             val range = DateTimeRange(
                 start = LocalDateTime.of(startDate, anyTime),
@@ -22,31 +22,35 @@ class DateTimeRangeTest : StringSpec() {
             range.isStartMatching(startDate.plusDays(1)) shouldBe false
         }
 
-        "isWithin for date and time" {
-            val start = LocalDateTime.of(2024, 12, 5, 14, 30)
-            val startDate = start.toLocalDate()
-            val startTime = start.toLocalTime()
-            val end = LocalDateTime.of(2024, 12, 5, 15, 30)
-            val endTime = end.toLocalTime()
-            val range = DateTimeRange(
-                start = start,
-                end = end,
-            )
-
-            range.isStartMatching(startDate, start.toLocalTime()) shouldBe true
-            range.isStartMatching(startDate.plusDays(1), timeFrom = startTime) shouldBe false
-            range.isStartMatching(startDate.minusDays(1), timeFrom = startTime) shouldBe false
-            range.isStartMatching(startDate, timeFrom = startTime.minusMinutes(1)) shouldBe true
-            range.isStartMatching(startDate, timeFrom = startTime.plusMinutes(1)) shouldBe false
-            range.isStartMatching(startDate, timeFrom = startTime, timeTo = endTime) shouldBe true
+        val start = LocalDateTime.of(2024, 12, 5, 14, 30)
+        val startDate = start.toLocalDate()
+        val startTime = start.toLocalTime()
+        val end = LocalDateTime.of(2024, 12, 5, 15, 30)
+        val endTime = end.toLocalTime()
+        val range = DateTimeRange(
+            start = start,
+            end = end,
+        )
+        "isStartMatching for date and time - timeFrom and timeTo" {
             range.isStartMatching(
                 startDate,
                 timeFrom = startTime.minusHours(1),
                 timeTo = endTime.plusHours(1)
             ) shouldBe true
-            range.isStartMatching(startDate, timeTo = endTime) shouldBe true
-            range.isStartMatching(startDate, timeTo = endTime.plusMinutes(1)) shouldBe true
-            range.isStartMatching(startDate, timeTo = endTime.minusMinutes(1)) shouldBe false
+
+        }
+        "isStartMatching for date and time - timeFrom" {
+            range.isStartMatching(startDate, timeFrom = start.toLocalTime()) shouldBe true
+            range.isStartMatching(startDate.plusDays(1), timeFrom = startTime) shouldBe false
+            range.isStartMatching(startDate.minusDays(1), timeFrom = startTime) shouldBe false
+            range.isStartMatching(startDate, timeFrom = startTime.minusMinutes(1)) shouldBe true
+            range.isStartMatching(startDate, timeFrom = startTime.plusMinutes(1)) shouldBe false
+            range.isStartMatching(startDate, timeFrom = startTime, timeTo = endTime) shouldBe true
+        }
+        "isStartMatching for date and time - timeTo" {
+            range.isStartMatching(startDate, timeTo = startTime) shouldBe true
+            range.isStartMatching(startDate, timeTo = startTime.plusMinutes(1)) shouldBe true
+            range.isStartMatching(startDate, timeTo = startTime.minusMinutes(1)) shouldBe false
         }
     }
 }
