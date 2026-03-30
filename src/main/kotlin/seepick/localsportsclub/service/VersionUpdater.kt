e9668cf78c5351a8ac5da931a57f33b951bbf535
Seepick <seepick@users.noreply.github.com>
2026-03-30 15:30:34 +0200

    dynamic script

--- src/main/kotlin/seepick/localsportsclub/service/VersionUpdater.kt
+++ src/main/kotlin/seepick/localsportsclub/service/VersionUpdater.kt
@@ -1,27 +1,41 @@
 package seepick.localsportsclub.service
 
-import io.github.oshai.kotlinlogging.KotlinLogging
+import io.github.oshai.kotlinlogging.KotlinLogging.logger
 import java.awt.Desktop
+import java.io.File
 import java.net.URI
 
 object VersionUpdater {
 
-    private val log = KotlinLogging.logger {}
+    private val log = logger {}
     private val latestDownloadUrl =
         "https://github.com/seepick/LocalSportsClub/releases/latest/download/LocalSportsClub.${OsSniffer.os.installerSuffix}"
-    private val scriptLocation = "/Applications/LocalSportsClub.app/Contents/Resources/update.scpt"
 
     fun downloadLatestVersion() {
-//        if (OsSniffer.os == OsSniffer.Os.mac && File(scriptLocation).exists()) {
-//            autoUpdate()
-//        } else {
-        log.debug { "Downloading latest version at: $latestDownloadUrl" }
-        Desktop.getDesktop().browse(URI(latestDownloadUrl))
-//        }
+        if (OsSniffer.os == OsSniffer.Os.mac) {
+            log.debug { "Auto-updating application for macOS by using update script." }
+            autoUpdate()
+            log.info { "Script started. It will request an application quit..." }
+        } else {
+            log.debug { "Downloading latest version from: $latestDownloadUrl" }
+            Desktop.getDesktop().browse(URI(latestDownloadUrl))
+        }
     }
 
     private fun autoUpdate() {
-        log.info { "Executing shell script located at: $scriptLocation" }
+        val script = copyScript()
+        executeScriptInBg(script.absolutePath)
+    }
+
+    private fun copyScript(): File {
+        val input = openFromClasspath("/update.scpt")
+        val target = File.createTempFile("update", ".scpt")
+        target.writeBytes(input.readAllBytes())
+        return target
+    }
+
+    private fun executeScriptInBg(scriptLocation: String) {
+        log.info { "Executing applescript located at: $scriptLocation" }
         ProcessBuilder("sh", "-c", "nohup osascript $scriptLocation &")
             .redirectOutput(ProcessBuilder.Redirect.DISCARD)
             .redirectError(ProcessBuilder.Redirect.DISCARD)
